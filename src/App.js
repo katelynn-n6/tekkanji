92c5774dc0d391340a61dbb060bfca2e2edb979a
katelynn-n6 <katelynnnguyen6@gmail.com>
2022-08-08 11:21:55 -0400

    first commit
    home + learn page completely built

---
+++ src/App.js
@@ -0,0 +1,39 @@
+//import "./App.css";
+import { Route, Routes, Link, useNavigate } from "react-router-dom";
+import Learn from "./Pages/Learn";
+import Home from "./Pages/Home";
+import Review from "./Pages/Review";
+import About from "./Pages/About";
+import React from "react";
+
+function App() {
+  //<h3>a kanji learning system for JAPN at Georgia Tech</h3>
+
+  let navigate = useNavigate();
+  const moveAbout = () => {
+    navigate("about");
+  };
+
+  return (
+    <div>
+      <div className="App">
+        <div className="header">
+          <h1>tekkanji</h1>
+          <h2>テック + かんじ</h2>
+        </div>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/learn" element={<Learn />} />
+          <Route path="/review" element={<Review />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+      <div id="about" className="about">
+        <div onClick={moveAbout}>about</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/About.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { useNavigate } from "react-router-dom";
+
+function About() {
+  let navigate = useNavigate();
+  const navigateHome = () => {
+    navigate("/");
+    document.getElementById("about").style.display = "block";
+  };
+
+  document.getElementById("about").style.display = "none";
+
+  return (
+    <div className="App">
+      <h3>about</h3>
+      <div className="about-info">
+        <div>
+          <p>built by Katelynn Nguyen</p>
+          <p>
+            {"supported by the "}
+            <a href="https://app.kanjialive.com/api/docs">KanjiAlive API</a>
+          </p>
+          <p>
+            {"supporting the "}
+            <a href="https://a2096-19034049.cluster208.canvas-user-content.com/courses/2096~1190/files/2096~19034049/course%20files/kanji/kanjilist-new.html">
+              322 kanji
+            </a>{" "}
+            taught in JAPN 1001-3001 at Georgia Tech
+          </p>
+        </div>
+      </div>
+      <div>
+        <button onClick={navigateHome}> home </button>
+      </div>
+    </div>
+  );
+}
+
+export default About;
